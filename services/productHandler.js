4e5c4753bb84cf5cda82aaef014b209f47a4725c
nikoicarl <cnikoi@logig.tech>
2025-08-04 01:31:29 -0700

    handler changes

--- services/productHandler.js
+++ services/productHandler.js
@@ -5,8 +5,14 @@ const config = require('../config');
 async function getMultiple(page = 1) {
     const offset = helper.getOffset(page, config.listPerPage);
     const rows = await db.query(
-        `SELECT *
-    FROM product WHERE status = 'active' LIMIT ${offset},${config.listPerPage}`
+        `SELECT 
+        product.*, 
+        CONCAT(user.first_name, ' ', user.last_name) AS supplier
+    FROM product
+    INNER JOIN user ON product.userid = user.userid
+    WHERE product.status = ?
+    LIMIT ?, ?`,
+        ['active', offset, config.listPerPage]
     );
     const data = helper.emptyOrRows(rows);
     const meta = { page };
--- services/serviceHandler.js
+++ services/serviceHandler.js
@@ -5,9 +5,16 @@ const config = require('../config');
 async function getMultiple(page = 1) {
     const offset = helper.getOffset(page, config.listPerPage);
     const rows = await db.query(
-        `SELECT *
-    FROM service WHERE status = 'active' LIMIT ${offset},${config.listPerPage}`
+        `SELECT 
+        service.*, 
+        CONCAT(user.first_name, ' ', user.last_name) AS supplier
+    FROM service
+    INNER JOIN user ON service.userid = user.userid
+    WHERE service.status = ?
+    LIMIT ?, ?`,
+        ['active', offset, config.listPerPage]
     );
+
     const data = helper.emptyOrRows(rows);
     const meta = { page };
 
